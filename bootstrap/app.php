7d449f63b0f6db83e906967e041132a677f079df
unknown <yelimuratovsalimbay@gmail.com>
2024-11-21 03:54:19 -0800

    Middlewares added successfully

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\Auth;
+use App\Http\Middleware\Guest;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // alias middleware
+        $middleware->alias([
+            'auth' => Auth::class,
+            'guest' => Guest::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -10,32 +10,36 @@ use App\Http\Controllers\TaskController;
 
 
 // Auth routes
-Route::get('/',[AuthController::class,'login_page'])->name('login.index');
-Route::post('/login',[AuthController::class,'login'])->name('login');
-Route::get('/registration',[AuthController::class,'register_page'])->name('register.index');
-Route::post('/register',[AuthController::class,'register'])->name('register');
+Route::middleware(['guest'])->group(function () {
+    Route::get('/',[AuthController::class,'login_page'])->name('login.index');
+    Route::post('/login',[AuthController::class,'login'])->name('login');
+    Route::get('/registration',[AuthController::class,'register_page'])->name('register.index');
+    Route::post('/register',[AuthController::class,'register'])->name('register');
+});
 
-// User routes
-Route::get('/users', [UserController::class, 'index'])->name('user.index');
-Route::post('/users', [UserController::class, 'store'])->name('user.store');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('user.update');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('user.destroy');
-Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+Route::middleware(['auth'])->group(function () {
+    // User routes
+    Route::get('/users', [UserController::class, 'index'])->name('user.index');
+    Route::post('/users', [UserController::class, 'store'])->name('user.store');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('user.update');
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+    Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 
-// Category routes
-Route::get('/categories', [CategoryController::class, 'index'])->name('category.index');
-Route::post('/category/store', [CategoryController::class, 'store']);
-Route::put('/category/update', [CategoryController::class, 'update']);
-Route::delete('/category/delete/{category}', [CategoryController::class, 'destroy']);
+    // Category routes
+    Route::get('/categories', [CategoryController::class, 'index'])->name('category.index');
+    Route::post('/category/store', [CategoryController::class, 'store']);
+    Route::put('/category/update', [CategoryController::class, 'update']);
+    Route::delete('/category/delete/{category}', [CategoryController::class, 'destroy']);
 
-// Area routes
-Route::get('/areas', [AreaController::class, 'index']);
-Route::post('/area/store', [AreaController::class, 'store']);
-Route::put('/area/update', [AreaController::class, 'update']);
-Route::delete('/area/delete/{id}', [AreaController::class, 'destroy']);
+    // Area routes
+    Route::get('/areas', [AreaController::class, 'index']);
+    Route::post('/area/store', [AreaController::class, 'store']);
+    Route::put('/area/update', [AreaController::class, 'update']);
+    Route::delete('/area/delete/{id}', [AreaController::class, 'destroy']);
 
-// Task routes
-Route::resource('tasks', TaskController::class);
-Route::resource('taskAreas', TaskAreaController::class);
-Route::post('/task/store', [TaskController::class, 'store'])->name('task.store');
+    // Task routes
+    Route::resource('tasks', TaskController::class);
+    Route::resource('taskAreas', TaskAreaController::class);
+    Route::post('/task/store', [TaskController::class, 'store'])->name('task.store');
+});
